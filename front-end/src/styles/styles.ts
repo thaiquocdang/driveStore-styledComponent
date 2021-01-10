18e1a4135962718835e58f8cb857d06108c037ea
thaiquocdang <56788585+thaiquocdang@users.noreply.github.com>
2021-01-11 10:54:20 +1100

    add media queries template

--- front-end/src/styles/styles.ts
+++ front-end/src/styles/styles.ts
@@ -15,3 +15,17 @@ export const setFont = {
 export const setLetterSpacing = (number: number = 2) => {
   return `letter-spacing: ${number}px`
 }
+
+const sizes = {
+  large: 1200,
+  desktop: 992,
+  tablet: 768,
+  phone: 576,
+  miniPhone: 320,
+}
+//Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (style: String) =>
+    `@media (min-width: ${sizes[label]}) { ${style} }`
+  return acc
+}, {} as { [index: string]: Function })
